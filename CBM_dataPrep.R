6240566447b223edd3130dd4d8c9839a93816b3e
Susan Murray <murray.e.susan@gmail.com>
2025-06-03 13:51:52 -0700

    disturbance rasters can be retrieved any simList object using the disturbanceMeta 'objectName' column

--- CBM_dataPrep.R
+++ CBM_dataPrep.R
@@ -111,7 +111,8 @@ defineModule(sim, list(
       columns = c(
         eventID             = "Event type ID",
         disturbance_type_id = "Optional. CBM-CFS3 disturbance type ID. If not provided, the user will be prompted to choose IDs.",
-        name                = "Optional. Disturbance name (e.g. 'Wildfire'). Required if 'disturbance_type_id' absent."
+        name                = "Optional. Disturbance name (e.g. 'Wildfire'). Required if 'disturbance_type_id' absent.",
+        objectName          = "Optional. Name of the object in the `simList` to retrieve the disturbances from annually."
       )),
     expectsInput(
       objectName = "disturbanceMetaURL", objectClass = "character", desc = "URL for `disturbanceMeta`"),
@@ -201,14 +202,29 @@ doEvent.CBM_dataPrep <- function(sim, eventTime, eventType, debug = FALSE) {
 
     readDisturbances = {
 
+      # Get disturbances for the year
       if (length(sim$disturbanceRasters) > 0){
 
-        eventIDs <- names(sim$disturbanceRasters)
-        if (is.null(eventIDs))    stop("disturbanceRasters list names must be disturbance event IDs")
-        if (any(is.na(eventIDs))) stop("disturbanceRasters list names contains NAs")
+        distRasts <- lapply(sim$disturbanceRasters, function(d){
+          if (as.character(time(sim)) %in% names(d)) d[[as.character(time(sim))]]
+        })
+        distRasts <- distRasts[!sapply(distRasts, is.null)]
 
-        eventIDs <- suppressWarnings(tryCatch(as.integer(eventIDs), error = function(e) stop(
-          "disturbanceRasters list names must be coercible to integer")))
+      }else distRasts <- list()
+
+      # Retrieve disturbances from simList
+      for (i in which(!is.na(sim$disturbanceMeta$objectName))){
+
+        distRasts[[as.character(sim$disturbanceMeta[i,]$eventID)]] <- get(
+          sim$disturbanceMeta[i,]$objectName, envir = sim)
+      }
+
+      # Summarize year events into a table
+      if (length(distRasts) > 0){
+
+        if (is.null(names(distRasts)))    stop("disturbanceRasters list names must be disturbance event IDs")
+        if (any(is.na(names(distRasts)))) stop("disturbanceRasters list names contains NAs")
+        if (any(names(distRasts) == ""))  stop("disturbanceRasters list names contains empty strings")
 
         if (is.null(sim$disturbanceEvents)){
           sim$disturbanceEvents <- data.table::data.table(
@@ -218,31 +234,31 @@ doEvent.CBM_dataPrep <- function(sim, eventTime, eventType, debug = FALSE) {
           )
         }
 
-        # Summarize year events into a table
-        for (i in 1:length(sim$disturbanceRasters)){
-          if (as.character(time(sim)) %in% names(sim$disturbanceRasters[[i]])){
+        eventIDs <- suppressWarnings(tryCatch(
+          as.integer(names(distRasts)),
+          error = function(e) stop("disturbanceRasters list names must be coercible to integer")))
 
-            distAlign <- prepInputsToMasterRaster(
-              sim$disturbanceRasters[[i]][[as.character(time(sim))]],
-              sim$masterRaster
-            ) |> Cache()
+        for (i in 1:length(distRasts)){
+          distAlign <- prepInputsToMasterRaster(
+            distRasts[[i]],
+            sim$masterRaster
+          ) |> Cache()
 
-            eventIndex <- which(!is.na(terra::values(distAlign)[,1]))
-            if (length(eventIndex) > 0){
-              sim$disturbanceEvents <- rbind(sim$disturbanceEvents, data.table::data.table(
-                pixelIndex = eventIndex,
-                year       = as.integer(time(sim)),
-                eventID    = eventIDs[[i]]
-              ))
-            }
+          eventIndex <- which(!is.na(terra::values(distAlign)[,1]))
+          if (length(eventIndex) > 0){
+            sim$disturbanceEvents <- rbind(sim$disturbanceEvents, data.table::data.table(
+              pixelIndex = eventIndex,
+              year       = as.integer(time(sim)),
+              eventID    = eventIDs[[i]]
+            ))
+          }
 
-            if (P(sim)$saveRasters){
-              outPath <- file.path(
-                outputPath(sim), "CBM_dataPrep",
-                sprintf("distRast_%s-%s_%s.tif", i, eventIDs[[i]], time(sim)))
-              dir.create(dirname(outPath), recursive = TRUE, showWarnings = FALSE)
-              terra::writeRaster(distAlign, outPath, overwrite = TRUE)
-            }
+          if (P(sim)$saveRasters){
+            outPath <- file.path(
+              outputPath(sim), "CBM_dataPrep",
+              sprintf("distRast_%s-%s_%s.tif", i, eventIDs[[i]], time(sim)))
+            dir.create(dirname(outPath), recursive = TRUE, showWarnings = FALSE)
+            terra::writeRaster(distAlign, outPath, overwrite = TRUE)
           }
         }
       }
--- tests/testthat/test-2-module_1-SK-small.R
+++ tests/testthat/test-2-module_1-SK-small.R
@@ -15,6 +15,8 @@ test_that("Module: SK-small", {
 
     SpaDES.project::setupProject(
 
+      times = list(start = 1998, end = 2000),
+
       modules = "CBM_dataPrep",
       paths   = list(
         projectPath = projectPath,
@@ -34,6 +36,27 @@ test_that("Module: SK-small", {
         extent     = c(xmin = -687696, xmax = -681036, ymin = 711955, ymax = 716183),
         resolution = 30,
         vals       = 1
+      ),
+
+      # Set disturbances
+      ## Test matching user disturbances with CBM-CFS3 disturbances
+      disturbanceMeta = rbind(
+        data.frame(eventID = 1, wholeStand = 1, name = "Wildfire", objectName = NA_character_),
+        data.frame(eventID = 2, wholeStand = 1, name = "Clearcut harvesting without salvage", objectName = "clearcut")
+      ),
+      disturbanceRasters = list(
+        `1` = list(
+          `1998` = terra::rast(
+            crs        = "EPSG:3979",
+            extent     = c(xmin = -687696, xmax = -681036, ymin = 711955, ymax = 716183),
+            resolution = 30,
+            vals       = c(1, rep(NA, 31301))
+        ))),
+      clearcut = terra::rast(
+        crs        = "EPSG:3979",
+        extent     = c(xmin = -687696, xmax = -681036, ymin = 711955, ymax = 716183),
+        resolution = 30,
+        vals       = c(1, rep(NA, 31301))
       )
     )
   )
@@ -87,6 +110,41 @@ test_that("Module: SK-small", {
   expect_equal(simTest$cohortDT$pixelIndex, 1:31302)
   expect_equal(simTest$cohortDT$cohortID, simTest$cohortDT$pixelIndex)
 
+
+  ## Check output 'disturbanceMeta' ----
+
+  expect_true(!is.null(simTest$disturbanceMeta))
+  expect_true(inherits(simTest$disturbanceMeta, "data.table"))
+
+  expect_equal(nrow(simTest$disturbanceMeta), 2)
+
+  # Check that disturbances have been matched correctly
+  expect_equal(simTest$disturbanceMeta$disturbance_type_id, c(1, 204))
+
+
+  ## Check output 'disturbanceEvents' ----
+
+  expect_true(!is.null(simTest$disturbanceEvents))
+  expect_true(inherits(simTest$disturbanceEvents, "data.table"))
+
+  for (colName in c("pixelIndex", "year", "eventID")){
+    expect_true(colName %in% names(simTest$disturbanceEvents))
+    expect_true(is.integer(simTest$disturbanceEvents[[colName]]))
+    expect_true(all(!is.na(simTest$disturbanceEvents[[colName]])))
+  }
+
+  distEvents <- split(simTest$disturbanceEvents, simTest$disturbanceEvents$eventID)
+  expect_identical(names(distEvents), c("1", "2"))
+
+  expect_equal(distEvents[["1"]], rbind(
+    data.table(pixelIndex = 1, year = 1998, eventID = 1)
+  ))
+
+  expect_equal(distEvents[["2"]], rbind(
+    data.table(pixelIndex = 1, year = 1998, eventID = 2),
+    data.table(pixelIndex = 1, year = 1999, eventID = 2),
+    data.table(pixelIndex = 1, year = 2000, eventID = 2)
+  ))
 })
 
 
