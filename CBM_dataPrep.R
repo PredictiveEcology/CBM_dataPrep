026099d35c36c0d606c5d80e3c528d80b964ff4f
Susan Murray <murray.e.susan@gmail.com>
2025-11-18 09:29:00 -0800

    CBMutils >=2.4.1.9005 required

--- CBM_dataPrep.R
+++ CBM_dataPrep.R
@@ -18,7 +18,7 @@ defineModule(sim, list(
   reqdPkgs = list(
     "data.table", "RSQLite", "sf", "terra", "exactextractr", "gstat",
     "reproducible (>=2.1.2)", "digest", "googledrive",
-    "PredictiveEcology/CBMutils@development (>=2.4.1.9004)",
+    "PredictiveEcology/CBMutils@development (>=2.4.1.9005)",
     "PredictiveEcology/LandR@development"
   ),
   parameters = rbind(
