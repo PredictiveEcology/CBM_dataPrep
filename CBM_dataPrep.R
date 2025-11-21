8de3c3ee9e47b811a8a5211fe8f0f2e80e983fb4
Susan Murray <murray.e.susan@gmail.com>
2025-11-21 15:15:12 -0800

    parameter data type update

--- CBM_dataPrep.R
+++ CBM_dataPrep.R
@@ -24,8 +24,10 @@ defineModule(sim, list(
   parameters = rbind(
     defineParameter("saveRasters", "logical", FALSE, NA, NA, "Save rasters of inputs aligned to the `masterRaster`"),
     defineParameter("ageBacktrack", "list", NA, NA, NA, "Age backtracking parameters"),
-    defineParameter("parallel.cores",     "integer", NA,    NA, NA, "Number of cores to use in parallel processing"),
-    defineParameter("parallel.chunkSize", "integer", 25000, NA, NA, "Chunk size to use in parallel processing"),
+    defineParameter("parallel.cores",     "integer", NA_integer_, NA, NA,
+                    "Number of cores to use in parallel processing"),
+    defineParameter("parallel.chunkSize", "integer", 25000L, NA, NA,
+                    "Chunk size to use in parallel processing"),
     defineParameter(".useCache", "character", "init", NA, NA, "Cache module events")
   ),
   inputObjects = bindrows(
